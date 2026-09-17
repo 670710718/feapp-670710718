bd29c238f56fe2d558fc1d9b3cf0f5e10a5c0c6e
Natchaphon Sakolkitrungroj <sakolkitrungroj_n@su.ac.th>
2026-09-17 05:17:14 +0000

    new state

---
+++ my-app/src/components/GenreBox.jsx
@@ -0,0 +1,22 @@
+//import { useState } from 'react';
+
+
+// const [genre, setGenre] = useState('Sci-Fi');
+// const [watched, setWatched] = useState(false);
+
+function GenreBox({genre ,onGenreChange,genres}) 
+{
+    return (
+   <select value={genre} onChange={(e) => onGenreChange(e.target.value)}
+    className="rounded-lg border border-slate-300 px-3 py-2">
+        <option value="all">ทุกประเภท</option>
+        {genres.map(g => (
+            <option key={g} value={g}>{g}</option>
+        ))}
+    
+    </select>
+    )
+
+}
+
+export default  GenreBox;
